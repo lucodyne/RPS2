a18b366decb9f7dc0335753c962f19ef797c432f
Sean Lim <lim_s@outlook.com>
2019-09-08 18:29:17 -0500

    successfully send/update water/fire/grass

--- assets/script/game.js
+++ assets/script/game.js
@@ -38,7 +38,7 @@ const instance = {
   playerRPSChoice: "none"
 };
 
-// creates the page once instead of on every update
+// creates page 1
 instance.playerJoin = $("<div></div>");
 for (bannerMaker = 1; bannerMaker < 3; bannerMaker++) {
   const newBanner = $(
@@ -65,9 +65,8 @@ $(document).on("click", "#resetButton", function(event) {
 // bug: clicking reset during countdown will not stop timers
 function resetGame() {
   console.log("game reset");
-  $("#timer").empty();
-  $(".gray").removeClass("gray");
   instance.playerNumber = 0;
+  instance.RPSMenu = "";
   database.ref(`/gameRoom1`).update({
     gameReset: false,
     gameState: "genSelect",
@@ -80,6 +79,8 @@ function resetGame() {
     player2Selected: "none",
     player2Wins: 0
   });
+  $("#timer").empty();
+  $(".gray").removeClass("gray");
 }
 
 // adds listener to player select divs
@@ -108,41 +109,6 @@ function genSelectListeners() {
         }
       });
   });
-}
-
-// phase 1 timer:
-function startTimer() {
-  let countDownTimer = 4;
-  const startCountDown = setInterval(() => {
-    $("#prompt").text(`BOTH PLAYERS READY!`);
-    countDownTimer--;
-    if (countDownTimer > 0) {
-      $("#timer").text(`GAME STARTING IN ${countDownTimer}...`);
-    } else {
-      clearInterval(startCountDown);
-      database.ref("/gameRoom1").update({
-        gameState: "rockPaperScissors"
-      });
-    }
-  }, 1000);
-}
-
-// phase 2 timer
-function roundTimer() {
-  $("#prompt").text("CHOOSE A POKEMON!");
-  let RPSTimer = 5;
-  $("#timer").text(`TIME REMAINING: ${RPSTimer}`);
-  const RPSCountDown = setInterval(() => {
-    RPSTimer--;
-    if (RPSTimer > 0) {
-      $("#timer").text(`TIME REMAINING: ${RPSTimer}`);
-    } else {
-      clearInterval(RPSCountDown);
-      database.ref("/gameRoom1").update({
-        gameState: "results"
-      });
-    }
-  }, 1000);
 }
 
 database.ref().on("value", function(stateUpdate) {
@@ -185,17 +151,100 @@ database.ref().on("value", function(stateUpdate) {
         stateUpdate.val().gameRoom1.player1Entered &&
         stateUpdate.val().gameRoom1.player2Entered === true
       ) {
+        // phase 1 timer:
+        function startTimer() {
+          let countDownTimer = 4;
+          const startCountDown = setInterval(() => {
+            countDownTimer--;
+            if (countDownTimer > 0) {
+              $("#prompt").text(`BOTH PLAYERS READY!`);
+              $("#timer").text(`GAME STARTING IN ${countDownTimer}...`);
+            } else {
+              clearInterval(startCountDown);
+              database.ref("/gameRoom1").update({
+                gameState: "rockPaperScissors"
+              });
+            }
+          }, 1000);
+        }
         startTimer();
       }
       // phase 2: selecting rock paper or scissors, will alternate between this and the results page until reset
     } else if (stateUpdate.val().gameRoom1.gameState === "rockPaperScissors") {
       // change page to rock paper scissors buttons
-      $();
+      // do this with a function, relies on playerid
+      if (
+        stateUpdate.val().gameRoom1[`player${instance.playerNumber}ID`] ===
+        instance.playerID
+      ) {
+        if (instance.RPSMenu === "") {
+          function createRPSMenu() {
+            instance.RPSMenu = $("<div id=RPSMenu></div>")
+              .append(
+                `<img id=grass class=iChooseYou src="./assets/images/Grass${instance.playerNumber}.png"></img>`
+              )
+              .append(
+                `<img id=fire class=iChooseYou src="./assets/images/Fire${instance.playerNumber}.png"></img>`
+              )
+              .append(
+                `<img id=water class=iChooseYou src="./assets/images/Water${instance.playerNumber}.png"></img>`
+              );
+            $("#mainContent").html(instance.RPSMenu);
+          }
+          createRPSMenu();
+        }
+      }
+
+      // function for adding listeners to pokemon
+      function addPokeListeners() {
+        $(document).on("click", ".iChooseYou", function() {
+          if (instance.playerNumber === 1) {
+            database.ref(`/gameRoom1`).update({
+              player1Selected: event.target.id
+            });
+          }
+          if (instance.playerNumber === 2) {
+            database.ref(`/gameRoom1`).update({
+              player2Selected: event.target.id
+            });
+          }
+          $(".choseYou").removeClass("choseYou");
+          $(event.target).addClass("choseYou");
+        });
+      }
+      addPokeListeners();
 
       // will only call roundTimer if not already running:
       if (stateUpdate.val().gameRoom1.roundTimer === false) {
+        // phase 2 timer
+        function roundTimer() {
+          database.ref("gameRoom1").update({
+            roundTimer: true,
+            player1Selected: "none",
+            player2Selected: "none"
+          });
+          $("#prompt").text("CHOOSE A POKEMON!");
+          let RPSTimer = 5;
+          $("#timer").text(`TIME REMAINING: ${RPSTimer}`);
+          const RPSCountDown = setInterval(() => {
+            $("#prompt").text("CHOOSE A POKEMON!");
+            RPSTimer--;
+            if (RPSTimer > 0) {
+              $("#timer").text(`TIME REMAINING: ${RPSTimer}`);
+            } else {
+              clearInterval(RPSCountDown);
+              database.ref("/gameRoom1").update({
+                roundTimer: false,
+                gameState: "results"
+              });
+            }
+          }, 1000);
+        }
         roundTimer();
       }
+    } else if (stateUpdate.val().gameRoom1.gameState === "results") {
+      console.log(`player1: ${stateUpdate.val().gameRoom1.player1Selected}`);
+      console.log(`player2: ${stateUpdate.val().gameRoom1.player2Selected}`);
     }
   }
 }); // closes database.ref().on("value"
